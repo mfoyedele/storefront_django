3b524a583dfd95ce3ce4cc37561da4becd46a3e8
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-19 18:15:59 +0100

    Update views.py

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    product = Product.objects.get(pk=2)
-    print(product)  
+    
+    product = Product.objects.get(pk=1)
+       
     
     return render(request, 'hello.html', {'name': 'Mosh'})
