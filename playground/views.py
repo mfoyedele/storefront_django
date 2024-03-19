d9d70e718c5721f8ba267d3bb1127bdc033da586
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-19 18:01:05 +0100

    get_query used

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from store.models import Product
 
 
 def say_hello(request):
-    x = calculate()
+    product = Product.objects.get(pk=2)
+    print(product)  
+    
     return render(request, 'hello.html', {'name': 'Mosh'})
