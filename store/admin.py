a02706f6e26e0ee61ffd943e9e17c795b0c07ff8
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-21 22:17:44 +0100

    list_editable functionality added

--- store/admin.py
+++ store/admin.py
@@ -4,6 +4,7 @@ from . import models
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
     
     
 admin.site.register(models.Collection)
