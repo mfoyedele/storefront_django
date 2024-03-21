14e33f04cf47cdd5ac7ea580c38eaafaff69d4a9
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-21 22:23:57 +0100

    CustomerAdmin functionality added to ModelAdmin class

--- store/admin.py
+++ store/admin.py
@@ -6,6 +6,13 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price']
     list_editable = ['unit_price']
     list_per_page = 10
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
     
     
 admin.site.register(models.Collection)
