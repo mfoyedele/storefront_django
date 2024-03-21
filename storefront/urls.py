75da7db38702566fc651cb610f53b12d4cbc32d4
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-21 21:54:24 +0100

    Renamed to admin

--- storefront/urls.py
+++ storefront/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 import debug_toolbar
 
 admin.site.site_header = 'Storefront Admin'
-
+admin.site.index_title = 'Admin'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
