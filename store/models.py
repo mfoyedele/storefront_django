55608755549ea1a98b17dd6eb4e6290fcf95ef3c
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-21 22:00:51 +0100

    def __str__ func added to collections model

--- store/models.py
+++ store/models.py
@@ -11,6 +11,8 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
