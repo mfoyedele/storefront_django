ab18e1307b51f2eed6b0dd12f8fd7734d3c5e447
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-22 20:18:04 +0100

    list_sect_related added for collection

--- store/admin.py
+++ store/admin.py
@@ -6,6 +6,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
     
     def collection_title(self, product):
         return product.collection.title  
