be8d477003cbc6db6aea2973861417b5d576cd37
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-22 20:15:54 +0100

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -3,10 +3,13 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
     
+    def collection_title(self, product):
+        return product.collection.title  
+      
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 10:
