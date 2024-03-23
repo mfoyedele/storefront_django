cf3d083a36ede7d855756cbf75c39ed6aaaada02
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-23 20:07:33 +0100

    blank set to true in product model

--- store/models.py
+++ store/models.py
@@ -20,7 +20,7 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
