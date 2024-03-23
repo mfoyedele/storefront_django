6e3189592cd1960969e796f6827560cf9cce8377
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-23 21:36:57 +0100

    Update store_custom app

--- store_custom/admin.py
+++ store_custom/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
+from store.models import Product
 from tags.models import TaggedItem
 
 
@@ -8,3 +9,10 @@ from tags.models import TaggedItem
 class TagInLine(GenericTabularInline):
     autocomplete_fields = ['tag']
     model = TaggedItem
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = ['TagInline']
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
