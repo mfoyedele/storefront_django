d12be7d6e54fe69e1abae82f353afeed29916f23
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-23 06:42:11 +0100

    update search_fields

--- store/admin.py
+++ store/admin.py
@@ -47,7 +47,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
-    search_fields = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
     
     
     @admin.display(ordering='orders_count')
