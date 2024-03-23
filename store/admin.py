b2e8779bd832226eb81168644fffb6ffb6cd1072
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-23 06:46:32 +0100

    list_filter functionality added to product model

--- store/admin.py
+++ store/admin.py
@@ -11,6 +11,7 @@ from . import models
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update']
     list_per_page = 10
     list_select_related = ['collection']
     
