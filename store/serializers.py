724e347fbc9dd07ad53157b9556d935e51505f9d
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-27 00:45:50 +0100

    Update ProductSerializer class

--- store/serializers.py
+++ store/serializers.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from rest_framework import serializers
 
 from store.models import Product
@@ -9,4 +10,4 @@ class ProductSerializer(serializers.Serializer):
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     
     def calculate_tax(self, product: Product):
-        return product.unit_price * 1.1
+        return product.unit_price * Decimal(1.1)
