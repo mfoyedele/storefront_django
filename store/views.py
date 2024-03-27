ddb155caba95170bb8fafab7e0b75838ac36000a
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-27 01:14:46 +0100

    collection_detail field added

--- store/views.py
+++ store/views.py
@@ -18,4 +18,8 @@ def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+@api_view
+def collection_detail(request, id):
+    return Response('ok')
     
