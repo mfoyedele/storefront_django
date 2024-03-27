b21742b573864269a1baee7249a24d3a7e22830f
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-27 19:10:32 +0100

    create method added

--- store/serializers.py
+++ store/serializers.py
@@ -30,3 +30,9 @@ class ProductSerializer(serializers.ModelSerializer):
     
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+    
+    def create(self, validated_data):
+        product = Product(**validated_data)
+        product.other = 1
+        product.save()
+        return product
