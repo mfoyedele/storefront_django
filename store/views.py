c0665d3593ce04a8333b80aa4523896fb06b6315
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-29 20:45:34 +0100

    Update ProductDetail class

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.db.models.aggregates import Count
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,7 +18,10 @@ class ProductList(ListCreateAPIView):
         return {'request': self.request}
     
     
-class ProductDetail(APIView):
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
     def get(self, request, id):
         product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
