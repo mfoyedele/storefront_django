db62c17047736a1ffeb2ed4ee98d4fef03b1b7b0
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-29 20:05:06 +0100

    Import mixins from django rest framework

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.db.models.aggregates import Count
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
