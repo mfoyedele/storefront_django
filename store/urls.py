41280903a8bb5d2099d47c352207e2f1f8f3d152
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-29 22:16:18 +0100

    Update store urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 from . import views
+from pprint import pprint
+
 
 router = SimpleRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
-router.urls
+pprint(router.urls)
 
 # URLConf
 urlpatterns = [
