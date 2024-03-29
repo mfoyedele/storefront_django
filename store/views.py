413cd04a6464b473b74e3ccb24d438aec434650d
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-29 22:02:51 +0100

    Update ProductViewSet class

--- store/views.py
+++ store/views.py
@@ -18,18 +18,13 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
     
-    
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if product.orderitems.count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
+  
 
 class CollectionList(ListCreateAPIView):
     queryset = Collection.objects.annotate(
