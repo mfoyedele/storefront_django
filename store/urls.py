a07aba2ac62d966a05045650d344d8ba9edaa9d1
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-03-29 22:12:11 +0100

    Update store urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from . import views
+
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+router.urls
 
 # URLConf
 urlpatterns = [
-    path('products/', views.ProductViewSet.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
+    # path('products/', views.ProductViewSet.as_view()),
+    # path('products/<int:pk>/', views.ProductDetail.as_view()),
+    # path('collections/', views.CollectionList.as_view()),
+    # path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
 ]
