04a1e6f54576dc140969baa9c601c2209b8b3ad1
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-04-02 05:16:05 +0100

    Update Review class

--- store/models.py
+++ store/models.py
@@ -101,3 +101,6 @@ class CartItem(models.Model):
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
