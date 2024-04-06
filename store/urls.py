f6e6d09cde5b500970ca3a73c0040ea851e17ffa
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-04-06 20:05:20 +0100

    Update store urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
+from django.urls.conf import include
+from rest_framework_nested import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
