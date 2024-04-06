19b4bf810df6b2af146387048a7f098841b629eb
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-04-06 21:27:27 +0100

    Update store serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -37,3 +37,8 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ['id', 'date', 'name', 'description']
+        
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id=product_id, **validated_data)
+       
