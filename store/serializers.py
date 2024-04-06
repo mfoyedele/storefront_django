7bded566b9ceea3171703ec9a6cb3dec77742f97
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-04-06 21:19:00 +0100

    Update store serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -36,4 +36,4 @@ class ProductSerializer(serializers.ModelSerializer):
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ['id', 'date', 'name', 'description', 'product']
+        fields = ['id', 'date', 'name', 'description']
