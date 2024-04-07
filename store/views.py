61a21cf68198962a7049ff8ee4947299206aec2f
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-04-07 07:42:21 +0100

    Update views.py

--- store/views.py
+++ store/views.py
@@ -15,7 +15,11 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     
     def get_queryset(self):
-        return Product.objects.filter(collection_id=self.request.query_params['collection_id'])
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params['collection_id']
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+        return queryset
     
     def get_serializer_context(self):
         return {'request': self.request}
