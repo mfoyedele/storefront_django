3c6459bc17e7e4cb69a5b53ab8fc48c07001c69d
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-04-20 22:26:19 +0100

    Update serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -71,6 +71,10 @@ class CartSerializer(serializers.ModelSerializer):
 class AddCartItemSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField()
     
+    def validate_product_id(self, value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError('No product with the given ID was found.')
+    
     def save(self, **kwargs):
         cart_id = self.context['cart_id']
         product_id = self.validated_data['product_id']
