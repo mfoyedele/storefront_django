bc1e26ef2c02e3f51fd207b38d64a67be62a1a58
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-04-27 22:11:32 +0100

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,9 @@ from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from store.models import Product
 from tags.models import TaggedItem
+from .models import User
 
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
     pass
 
