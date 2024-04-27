614994765af6f6004504fa19c78bd3d0f38aab91
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-04-27 22:09:37 +0100

    Upsate core admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from store.models import Product
 from tags.models import TaggedItem
 
+class UserAdmin()
 
-# Register your models here.
 class TagInline(GenericTabularInline):
     autocomplete_fields = ['tag']
     model = TaggedItem
