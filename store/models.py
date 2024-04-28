861c41f4ea0bedd61a234c1d6761454a831fb634
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-04-28 20:15:51 +0100

    Update store models.py

--- store/models.py
+++ store/models.py
@@ -56,9 +56,14 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
-    
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
+    
+    def first_name(self):
+        return self.user.first_name
+    
+    def last_name(self):
+        return self.user.last_name
     
     class Meta:
         ordering = ['user__first_name', 'user__last_name']
